282f25e1cb5c203fb58f6a488b446fcfe188f158
josevelez08 <jose-0919@hotmail.com>
2021-09-09 17:27:04 -0500

    adding questions to database

--- modules/questions/actions/updateQuestionData/updateQuestionData.js
+++ modules/questions/actions/updateQuestionData/updateQuestionData.js
@@ -2,14 +2,18 @@ const { users } = require(__basedir + "/db/controllers");
 const { throwBadRequestError } = require(__basedir + "/errors");
 const { messages } = require(__basedir + "/config");
 
-const updateQuestionData = async (name,updates) => {
+const updateQuestionData = async (name, updates) => {
 
-        const user = await users.updateUserByName({ name: name}, {updates: updates});
+
+        console.log(name);
+        console.log(updates);
+
+        const user = await users.updateUserByName(name,updates);
         if (!user) {
             throwBadRequestError(messages.USER_ALREADY_EXISTS);
         }
         return {
-            user: result
+            user
         };
 
 };
