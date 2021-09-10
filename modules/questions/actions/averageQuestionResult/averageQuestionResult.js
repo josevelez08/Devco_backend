deca8efff4b3ff734407b458711c3e8fae04f181
josevelez08 <jose-0919@hotmail.com>
2021-09-09 20:35:31 -0500

    Adding average function

--- modules/questions/actions/averageQuestionResult/averageQuestionResult.js
+++ modules/questions/actions/averageQuestionResult/averageQuestionResult.js
@@ -1,7 +1,30 @@
 const averageQuestionResult = (body) => {
-    
     let average = 0;
-    console.log(body);
+    if(body.questions1)
+    {
+        let questions1 = body.questions1;
+        questions1.forEach(element => {
+            if(element.answer === true) {
+                average +=4;
+            }
+            else {
+                average -=1;
+            }
+            
+        });
+    }
+    if(body.questions2)
+    {
+        let questions2 = body.questions2;
+        questions2.forEach(element => {
+            if(element.answer === true) {
+                average +=4;
+            }
+            else {
+                average -=1;
+            }
+        });
+    }
 
     return average;
 
--- modules/questions/controller.js
+++ modules/questions/controller.js
@@ -1,14 +1,13 @@
-const { updateQuestionData } = require("./actions");
+const { updateQuestionData, averageQuestionResult } = require("./actions");
 const { constants } = require(__basedir + "/config");
 const { SUCCESS } = constants;
- const { averageQuestionData } = require("./actions");
 
 const updateQuestion = async (req, res, next) => {
     try {
         const {name} = req.body;
         const data = await updateQuestionData(name, req.body);
-        const average = averageQuestionData(req.body);
-        res.status(SUCCESS.CODE).send({ data });
+        const average = averageQuestionResult(req.body);
+        res.status(SUCCESS.CODE).send({ average });
         next();
     } catch (error) {
         res.status(error.code).send({
